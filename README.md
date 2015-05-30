106b9da34293158b31880d7a86286e4b0b31dddf
Vital Kudzelka <vital.kudzelka@gmail.com>
2015-05-30 14:38:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+jQuery Flyout
+=============
+
+Flyout is a piece of content that fly out of a parent when some action occurs,
+for example, when box is clicked.
+
+Usage
+-----
+
+```html
+<style>
+  .flyout {
+    display: none;
+  }
+  .flyout.is-opened {
+    display: block;
+  }
+</style>
+
+<div class="has-flyout">
+  <div class="js-flyout-open">Show flyout</div>
+  <div class="flyout">Hello <span class="js-flyout-close">× close</span></div>
+</div>
+
+<script>
+  (function() {
+    $('.has-flyout').flyout({trigger: {open: 'js-flyout-open',
+                                       close: 'js-flyout-close'}});
+  }());
+</script>
+```
+
+License
+-------
+
+Licensed under the [MIT license](http://mit-license.org/vitalk).
---
+++ index.js
@@ -0,0 +1,167 @@
+(function($, window, document, undefined) {
+  'use strict';
+
+  $.fn.flyout = function(parameters) {
+    var
+      query = arguments[0],
+      response;
+
+    this.each(function() {
+      var
+        options = $.extend(true, {}, $.fn.flyout.options, parameters),
+        $instance = $(this),
+        namespace = options.namespace,
+        instance = $instance.data(namespace),
+        module;
+
+      module = {
+        init: function(options) {
+          instance = module;
+          instance.options = options;
+          instance.debug('Initializing instance with options', options);
+          instance.bind.events();
+          $instance.data(namespace, instance);
+        },
+
+        debug: function() {
+          if (instance.options.debug) {
+            var debug = Function.prototype.bind.call(
+              console.log, console, '[' + instance.options.namespace + ']'
+            );
+            debug.apply(console, arguments);
+          }
+        },
+
+        is: {
+          opened: function() {
+            return $instance.hasClass(instance.options.className.opened);
+          },
+
+          closed: function() {
+            return !instance.is.opened();
+          }
+        },
+
+        open: function() {
+          module.debug('Opening flyout');
+          if (instance.is.closed()) {
+            $instance
+              .addClass(instance.options.className.opened)
+              .removeClass(instance.options.className.closed);
+          }
+        },
+
+        close: function() {
+          module.debug('Closing flyout');
+          if (instance.is.opened()) {
+            $instance
+              .addClass(instance.options.className.closed)
+              .removeClass(instance.options.className.opened);
+          }
+        },
+
+        toggle: function() {
+          if (instance.is.opened()) {
+            instance.close();
+          }
+
+          else {
+            instance.open();
+          }
+        },
+
+        bind: {
+          events: function() {
+            instance.debug('Binding events');
+
+            if (instance.options.trigger.open) {
+              $instance
+                .find(instance.options.trigger.open)
+                .on('click', module.event.open);
+            }
+
+            if (instance.options.trigger.close) {
+              $instance
+                .find(instance.options.trigger.close)
+                .on('click', module.event.close);
+            }
+          }
+        },
+
+        event: {
+          open: function(event) {
+            instance.open();
+            event.preventDefault();
+          },
+
+          close: function(event) {
+            instance.close();
+            event.preventDefault();
+          }
+        },
+
+        invoke: function(query, passedArguments, context) {
+          var
+            object = instance,
+            context = context || module,
+            maxDepth,
+            response,
+            found;
+
+          if (typeof query === 'string' && object !== undefined) {
+            query = query.split(/[\. ]/);
+            maxDepth = query.length - 1;
+
+            $.each(query, function(depth, value) {
+              if ($.isPlainObject(object[value]) && (depth != maxDepth)) {
+                object = object[value];
+              }
+              else if (object[value] !== undefined) {
+                found = object[value];
+                return false;
+              }
+              else {
+                return false;
+              }
+            });
+          }
+
+          if ($.isFunction(found)) {
+            response = found.apply(context, passedArguments);
+          }
+          else if (found !== undefined) {
+            response = found;
+          }
+
+          return response;
+        }
+      };
+
+      if (instance === undefined) {
+        module.init(options);
+      }
+      response = instance.invoke(query);
+    });
+
+    return (response !== undefined)
+      ? response
+      : this;
+  };
+
+
+  $.fn.flyout.options = {
+    namespace: 'flyout',
+    debug: false,
+
+    className: {
+      opened: 'is-opened',
+      closed: false
+    },
+
+    trigger: {
+      open: false,
+      close: false
+    }
+  };
+
+}(jQuery, window, document));
